2eb1d221c5a83d7b711513c0d7bbe574b6a7bea7
Alexandra <alexandra.transhuman@gmail.com>
2012-12-01 22:30:48 +0200

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '935c96207b236343f25560e1d206ac9ebb94f0899b5139dbe2c2e7ea123c466601607b2d38b414b3ad98f36129b83a873e8d023d0af1429773ed94973c7eb7ad'
---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+# ...
+Product.create(title: 'Pivoines',
+	description:
+		%{<p>
+			aaa
+		</p>},
+	image_url: 'cs.jpg',
+	price: 20.20)
+# ...
+Product.create(title: 'Pivoines2',
+	description:
+		%{<p>
+			bbb
+		</p>},
+	image_url: 'cs.jpg',
+	price: 30.20)
+# ...
