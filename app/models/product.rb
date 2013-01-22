abc090aefb63064a40f10702acb8cd45797968b5
Alexandra-Tatarenko <novacode2007@gmail.com>
2013-01-22 15:29:58 +0800

    22.01.13

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-  validates :title, :length => { :minimum => 10 }
+  validates_length_of :title, :within => 6..10, :too_long => "pick a shorter name!!!", :too_short => "pick a longer name!!!"
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'You can use only GIF, JPG or PNG.'
